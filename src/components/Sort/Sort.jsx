27ff4f8c964a7ae95b356a639fab024bd584a6bf
mustkov <dolzhenkov74@gmail.com>
2022-04-04 00:28:35 +0400

    feat: add link to main page

--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Sort({ sortCities, sortCompany }) {
   return (
     <div className="sort">
+      <Link
+        className="sort_link"
+        to={{
+          pathname: `/`,
+        }}
+      >
+        Главная
+      </Link>
       <h1 className="sort_name">Сортировка</h1>
       <a
         className="sort_button waves-effect waves-light btn"
@@ -10,10 +19,12 @@ export default function Sort({ sortCities, sortCompany }) {
       >
         по городу
       </a>
-      <a 
-      className="sort_button waves-effect waves-light btn"
-      onClick={() => sortCompany()}
-      >по компании</a>
+      <a
+        className="sort_button waves-effect waves-light btn"
+        onClick={() => sortCompany()}
+      >
+        по компании
+      </a>
     </div>
   );
 }
